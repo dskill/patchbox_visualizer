b66b7d7ef7b9b293ad013d4450860fab6f044a13
Drew <drewskillman@gmail.com>
2022-12-13 19:04:33 +0000

    fix up boot scripts

--- kiosk.sh
+++ kiosk.sh
@@ -3,10 +3,11 @@ xset s noblank
 xset s off
 xset -dpms
 
-unclutter -idle 0.5 -root &
+unclutter -idle 0.0 -root &
 
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
 
 http-server /home/patch/src/patchbox_visualizer &
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8081
+#TODO: Make the IP generated when this script starts
+/usr/bin/chromium-browser --noerrdialogs --disable-cursor-lock-for-test --disable-infobars --kiosk http://192.168.50.241:3000/?gui
