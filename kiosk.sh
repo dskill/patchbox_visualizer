664b8dedf6c0464d515e86055acae29e00987b52
Drew <drewskillman@gmail.com>
2022-12-13 20:06:44 +0000

    change idle

--- kiosk.sh
+++ kiosk.sh
@@ -3,7 +3,7 @@ xset s noblank
 xset s off
 xset -dpms
 
-unclutter -idle 0.0 -root &
+unclutter -idle 0.1 -root &
 
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
