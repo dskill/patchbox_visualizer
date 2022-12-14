eec45d9bc0edbf11dd4f62d95bfd22afcda857cd
Drew <drewskillman@gmail.com>
2022-12-14 00:53:29 +0000

    working on pi again

--- kiosk.sh
+++ kiosk.sh
@@ -8,6 +8,7 @@ unclutter -idle 0.1 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
 
-http-server /home/patch/src/patchbox_visualizer &
+#http-server /home/patch/src/patchbox_visualizer &
 #TODO: Make the IP generated when this script starts
+#sclang /home/patch/src/patchbox_visualizer/sc/main.scd &
 /usr/bin/chromium-browser --noerrdialogs --disable-cursor-lock-for-test --disable-infobars --kiosk http://192.168.50.241:3000/?gui
