860ec0290b239dcbae2c44b3dbb01355a983f434
Drew <drew@drewskillman.com>
2022-12-17 23:35:14 -0800

    Update kill-processes.sh

---
+++ kill-processes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Find and kill all processes with "sclang" in the name
+pids=$(pgrep sclang)
+if [ -n "$pids" ]; then
+  kill $pids
+fi
+
+# Find and kill all processes with "server.js" in the name
+pids=$(pgrep server.js)
+if [ -n "$pids" ]; then
+  kill $pids
+fi
