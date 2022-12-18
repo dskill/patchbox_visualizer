7f5e2be49a9c751c8c123401766bb9a62917f49d
Drew <drew@drewskillman.com>
2022-12-18 15:29:13 -0800

    Update kill-processes.sh

--- kill-processes.sh
+++ kill-processes.sh
@@ -6,6 +6,12 @@ if [ -n "$pids" ]; then
   kill $pids
 fi
 
+# Find and kill all processes with "scsynth" in the name
+pids=$(pgrep scsynth)
+if [ -n "$pids" ]; then
+  kill $pids
+fi
+
 # Find and kill all processes with "server.js" in the name
 pids=$(pgrep node)
 if [ -n "$pids" ]; then
