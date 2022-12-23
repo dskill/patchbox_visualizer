0887fc842eb3e3e94ecc2ec4732f541b7dcdf7fa
Drew <drew@drewskillman.com>
2022-12-22 21:42:12 -0800

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Install the wait-for-it script (if not already installed)
-if ! [ -x "$(command -v wait-for-it)" ]; then
-  apt-get update && apt-get install -y curl
-  curl -sL https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh -o /usr/local/bin/wait-for-it
-  chmod +x /usr/local/bin/wait-for-it
-fi
+echo "Starting Patchbox Visualizer..." > /home/patch/logs/pv_startup.log
 
-# Wait for the WiFi connection to be established
-wait-for-it wlan0:80
+# Loop until MY_IP is not null
+# this waits for the WIFI network to be up
+while [ -z "$MY_IP" ]; do
+  MY_IP=$(ip addr show wlan0 | grep -Po 'inet \K[\d.]+')
+  echo "Detected IP: $MY_IP" >> /home/patch/logs/pv_startup.log
+  sleep 1
+done
 
-MY_IP=$(ip addr show wlan0 | grep -Po 'inet \K[\d.]+')
-echo "IP: $MY_IP"
 xset s noblank
 xset s off
 xset -dpms
@@ -22,12 +20,11 @@ sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chr
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
 
 cd /home/patch/src/patchbox_visualizer
-pwd > /home/patch/logs/info.log
-echo "Detected IP: $MY_IP" >> /home/patch/logs/info.log
+pwd >> /home/patch/logs/pv_startup.log
 
 git pull # get the latest code
 npm install # hacky way to make sure we have the latest dependencies
 node /home/patch/src/patchbox_visualizer/server.js &> /home/patch/logs/server.log & # node server and OSC bridge
 sclang /home/patch/src/patchbox_visualizer/sc/main.scd &> /home/patch/logs/supercollider.log & # super collider
-/usr/bin/chromium-browser --noerrdialogs --disable-cursor-lock-for-test --disable-infobars --kiosk http://$MY_IP:3000 # web broweser
-#/usr/bin/chromium-browser --noerrdialogs --disable-cursor-lock-for-test --disable-infobars --kiosk http://$MY_IP:3000/?gui
+#/usr/bin/chromium-browser --noerrdialogs --disable-cursor-lock-for-test --disable-infobars --kiosk http://$MY_IP:3000 # web broweser
+/usr/bin/chromium-browser --noerrdialogs --disable-cursor-lock-for-test --disable-infobars --kiosk http://$MY_IP:3000/?gui
