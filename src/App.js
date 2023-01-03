23323b69494ec36b3f5dfeba30f91975bdff46aa
Drew <drew@drewskillman.com>
2023-01-03 00:03:45 -0800

    add ascii effect test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Leva, useControls } from 'leva'
 
--- src/Effects/ScopeDistortionEffect.js
+++ src/Effects/ScopeDistortionEffect.js
@@ -1,8 +1,62 @@
 
-import { useRef, useState, useEffect } from 'react'
+import { useEffect, useRef, useState, useMemo, useLayoutEffect } from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { ScopeMaterial } from './ScopeMaterial'
 import { useControls } from 'leva'
+import { AsciiEffect } from 'three-stdlib'
+
+
+function AsciiRenderer({
+  renderIndex = 1,
+  bgColor = 'black',
+  fgColor = 'white',
+  characters = ' .:-+*=%@#',
+  invert = true,
+  color = false,
+  resolution = 0.15
+}) {
+  // Reactive state
+  const { size, gl, scene, camera } = useThree()
+
+  // Create effect
+  const effect = useMemo(() => {
+    const effect = new AsciiEffect(gl, characters, { invert, color, resolution })
+    effect.domElement.style.position = 'absolute'
+    effect.domElement.style.top = '0px'
+    effect.domElement.style.left = '0px'
+    effect.domElement.style.pointerEvents = 'none'
+    return effect
+  }, [characters, invert, color, resolution])
+
+  // Styling
+  useLayoutEffect(() => {
+    effect.domElement.style.color = fgColor
+    effect.domElement.style.backgroundColor = bgColor
+  }, [fgColor, bgColor])
+
+  // Append on mount, remove on unmount
+  useEffect(() => {
+    gl.domElement.style.opacity = '0'
+    gl.domElement.parentNode.appendChild(effect.domElement)
+    return () => {
+      gl.domElement.style.opacity = '1'
+      gl.domElement.parentNode.removeChild(effect.domElement)
+    }
+  }, [effect])
+
+  // Set size
+  useEffect(() => {
+    effect.setSize(size.width, size.height)
+  }, [effect, size])
+
+  // Take over render-loop (that is what the index is for)
+  useFrame((state) => {
+    effect.render(scene, camera)
+  }, renderIndex)
+
+  // This component returns nothing, it is a purely logical
+}
+
 
 // smoothstep function
 // TODO just use a math module
@@ -65,6 +119,7 @@ function ScopeDistortionEffect({ waveformTexture, waveformRms, waveformRmsAccum,
   }, [])  // empty array means effect will only be applied once
 
   return (
+    <>
     <mesh scale={[width, height, 1]}>
       <planeGeometry />
       <scopeMaterial ref={ref}
@@ -73,6 +128,8 @@ function ScopeDistortionEffect({ waveformTexture, waveformRms, waveformRmsAccum,
         iWaveformTexture0={waveformTexture.texture}
       />
     </mesh>
+    <AsciiRenderer/>
+    </>
   )
 }
 
